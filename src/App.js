0317449a29eb5d3386acbe8a9c3e7897f557dd56
Ojas <ojas.modak23@gmail.com>
2023-01-21 13:21:54 +0530

    Integrated login page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
+import Loginn from "./components/Loginn";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
+        <Route path="loginnew" element={<Loginn />} />
       </Routes>
     </BrowserRouter>
   );
