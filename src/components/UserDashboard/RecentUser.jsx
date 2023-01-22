a10200db4abf25a67a2569219e2da403d90563e4
Bhavesh <122824805+BhaveshMukheja@users.noreply.github.com>
2023-01-22 13:17:53 +0530

    RecentUser

---
+++ src/components/UserDashboard/RecentUser.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const RecentUser = () => {
+  return (
+    <>
+
+                                <div className="col-xl-8 col-md-6">
+                                    <div className="card Recent-Users">
+                                        <div className="card-header">
+                                            <h5>Recent Users</h5>
+                                        </div>
+                                        <div className="card-block px-0 py-3">
+                                            <div className="table-responsive">
+                                                <table className="table table-hover">
+                                                    <tbody>
+                                                        <tr className="unread">
+                                                            <td><img className="rounded-circle" style="width:40px;" src="assets/images/user/avatar-1.jpg" alt="activity-user"/></td>
+                                                            <td>
+                                                                <h6 className="mb-1">Isabella Christensen</h6>
+                                                                <p className="m-0">Lorem Ipsum is simply…</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>11 MAY 12:56</h6>
+                                                            </td>
+                                                            <td><a href="#!" className="label theme-bg2 text-white f-12">Reject</a><a href="#!" className="label theme-bg text-white f-12">Approve</a></td>
+                                                        </tr>
+                                                        <tr className="unread">
+                                                            <td><img className="rounded-circle" style="width:40px;" src="assets/images/user/avatar-2.jpg" alt="activity-user"/></td>
+                                                            <td>
+                                                                <h6 className="mb-1">Mathilde Andersen</h6>
+                                                                <p className="m-0">Lorem Ipsum is simply text of…</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6 className="text-muted"><i className="fas fa-circle text-c-red f-10 m-r-15"></i>11 MAY 10:35</h6>
+                                                            </td>
+                                                            <td><a href="#!" className="label theme-bg2 text-white f-12">Reject</a><a href="#!" className="label theme-bg text-white f-12">Approve</a></td>
+                                                        </tr>
+                                                        <tr className="unread">
+                                                            <td><img className="rounded-circle" style="width:40px;" src="assets/images/user/avatar-3.jpg" alt="activity-user"/></td>
+                                                            <td>
+                                                                <h6 className="mb-1">Karla Sorensen</h6>
+                                                                <p className="m-0">Lorem Ipsum is simply…</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>9 MAY 17:38</h6>
+                                                            </td>
+                                                            <td><a href="#!" className="label theme-bg2 text-white f-12">Reject</a><a href="#!" className="label theme-bg text-white f-12">Approve</a></td>
+                                                        </tr>
+                                                        <tr className="unread">
+                                                            <td><img className="rounded-circle" style="width:40px;" src="assets/images/user/avatar-1.jpg" alt="activity-user"/></td>
+                                                            <td>
+                                                                <h6 className="mb-1">Ida Jorgensen</h6>
+                                                                <p className="m-0">Lorem Ipsum is simply text of…</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6 className="text-muted f-w-300"><i className="fas fa-circle text-c-red f-10 m-r-15"></i>19 MAY 12:56</h6>
+                                                            </td>
+                                                            <td><a href="#!" className="label theme-bg2 text-white f-12">Reject</a><a href="#!" className="label theme-bg text-white f-12">Approve</a></td>
+                                                        </tr>
+                                                        <tr className="unread">
+                                                            <td><img className="rounded-circle" style="width:40px;" src="assets/images/user/avatar-2.jpg" alt="activity-user"/></td>
+                                                            <td>
+                                                                <h6 className="mb-1">Albert Andersen</h6>
+                                                                <p className="m-0">Lorem Ipsum is simply dummy…</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>21 July 12:56</h6>
+                                                            </td>
+                                                            <td><a href="#!" className="label theme-bg2 text-white f-12">Reject</a><a href="#!" className="label theme-bg text-white f-12">Approve</a></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            
+
+                            
+    </>
+  )
+}
+
+export default RecentUser
